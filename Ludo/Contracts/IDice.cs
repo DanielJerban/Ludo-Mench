d2ecf736ff555c9b6c4ae485e278e6d8f54de64a
DanielJerban <DanielJerban@users.noreply.github.com>
2019-01-26 17:21:47 +0330

    Added IDice and IPawn interfaces

---
+++ Ludo/Contracts/IDice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludo.Contracts
+{
+    interface IDice
+    {
+        // Minimum number of the Dice 
+        int MinNum { get; }
+        // Maximum number of a dice 
+        int MaxNum { get; }
+
+        // Throw Dice Function 
+        int Throw(Random rnd);
+    }
+}
---
+++ Ludo/Contracts/IPawn.cs
@@ -0,0 +1,20 @@
+﻿using Ludo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludo.Contracts
+{
+    interface IPawn
+    {
+        /// <summary>
+        /// Move Pawn step by step 
+        /// </summary>
+        /// <param name="playground">A list of playground fields</param>
+        /// <param name="finish">A list of finish fields</param>
+        /// <param name="steps">Moving steps count which is equal to the Dice number</param>
+        void Move(IList<Field> playground, IList<Field> finish, int steps);
+    }
+}
