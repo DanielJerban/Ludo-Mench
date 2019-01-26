82c3843dd9b473c867e9da5f9c74932dca17f3a1
DanielJerban <DanielJerban@users.noreply.github.com>
2019-01-26 17:25:51 +0330

    Added Invalid Name and PlayerCount exception classes

---
+++ Ludo/Exceptions/InvalidNameException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludo.Exceptions
+{
+    public class InvalidNameException : ApplicationException
+    {
+        public InvalidNameException()
+        {
+        }
+
+        public InvalidNameException(string message) : base(message)
+        {
+        }
+
+        public InvalidNameException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidNameException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Ludo/Exceptions/InvalidPlayerCountException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludo.Exceptions
+{
+    public class InvalidPlayerCountException : ApplicationException
+    {
+        public InvalidPlayerCountException()
+        {
+        }
+
+        public InvalidPlayerCountException(string message) : base(message)
+        {
+        }
+
+        public InvalidPlayerCountException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidPlayerCountException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
